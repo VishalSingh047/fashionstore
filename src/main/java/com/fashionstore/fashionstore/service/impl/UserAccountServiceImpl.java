ebfca57218df768a3e4c92c4a9e4bd1ee2cfd928
Vishal Singh <vs997131@gmail.com>
2026-07-16 04:36:26 +0530

    Created User module architecture with DTO, Service and Controller

--- src/main/java/com/fashionstore/fashionstore/service/impl/UserAccountServiceImpl.java
+++ src/main/java/com/fashionstore/fashionstore/service/impl/UserAccountServiceImpl.java
@@ -1,20 +1,40 @@
 package com.fashionstore.fashionstore.service.impl;
 
 import com.fashionstore.fashionstore.dto.RegisterRequest;
+import com.fashionstore.fashionstore.entity.UserAccount;
+import com.fashionstore.fashionstore.enums.Role;
 import com.fashionstore.fashionstore.repository.UserAccountRepository;
 import com.fashionstore.fashionstore.service.UserAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserAccountServiceImpl implements UserAccountService {
 
     @Autowired
-    private UserAccountRepository repository;
+    private UserAccountRepository userAccountRepo;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     @Override
-    public String register(RegisterRequest request){
-        return "Registration API under development";
+    public String register(RegisterRequest request) {
+        if (userAccountRepo.findByEmail(request.getEmail()).isPresent()) {
+            return "Email already exists";
+        }
+
+        UserAccount user = new UserAccount();
+
+        user.setFullName(request.getFullName());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setPhone(request.getPhone());
+        user.setRole(Role.CUSTOMER);
+
+        userAccountRepo.save(user);
+
+        return "Registered Successfully";
     }
 
 }
